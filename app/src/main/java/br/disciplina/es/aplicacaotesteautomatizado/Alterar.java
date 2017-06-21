0a9c6db7301ad6a22253b538d308eba5dc8e520a
belmondorodrigues <belmondo.rodrigues@gmail.com>
2017-06-21 17:01:45 -0300

    Comitt da inserção de avisos de atenção da tela de alteração

--- app/src/main/java/br/disciplina/es/aplicacaotesteautomatizado/Alterar.java
+++ app/src/main/java/br/disciplina/es/aplicacaotesteautomatizado/Alterar.java
@@ -3,10 +3,13 @@ package br.disciplina.es.aplicacaotesteautomatizado;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import br.disciplina.es.aplicacaotesteautomatizado.database.BancoController;
 import br.disciplina.es.aplicacaotesteautomatizado.database.CriaBanco;
@@ -29,6 +32,9 @@ public class Alterar extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.altera);
 
+//        final TextView textAvisoTitulo = (TextView) findViewById(R.id.textViewAvisoTitulo2);
+//        final TextView textAvisoAutor = (TextView) findViewById(R.id.textViewAvisoAutor2);
+
         codigo = this.getIntent().getStringExtra("codigo");
 
         crud = new BancoController(getBaseContext());
@@ -39,6 +45,10 @@ public class Alterar extends Activity {
 
         alterar = (Button)findViewById(R.id.button2);
 
+//        final String tituloString = livro.getText().toString();
+//        final String autorString = autor.getText().toString();
+//        final String editoraString = editora.getText().toString();
+
         cursor = crud.carregaDadoById(Integer.parseInt(codigo));
         livro.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.TITULO)));
         autor.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.AUTOR)));
@@ -47,12 +57,17 @@ public class Alterar extends Activity {
         alterar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                crud.alteraRegistro(Integer.parseInt(codigo), livro.getText().toString(), autor.getText().toString(),
-                        editora.getText().toString());
-                Intent intent = new Intent(Alterar.this, Consulta.class);
-                startActivity(intent);
-                finish();
+
+
+
+                    crud.alteraRegistro(Integer.parseInt(codigo), livro.getText().toString(), autor.getText().toString(),
+                            editora.getText().toString());
+                    Intent intent = new Intent(Alterar.this, Consulta.class);
+                    startActivity(intent);
+                    finish();
+
             }
+
         });
 
 
